eaff134e0b9a85a4f701e9cb103cfe601235b7d6
etfeben <etfeben@gmail.com>
2025-02-06 21:10:02 -0500

    Third Project

---
+++ src/test/java/org/example/Sample.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Sample {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Again!!");
+
+        System.out.println("Chroma Tech");
+    }
+
+}
